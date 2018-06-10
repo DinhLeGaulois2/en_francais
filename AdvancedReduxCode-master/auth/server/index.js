41fa50fa2be3b8e31f61f99bd219a6c0ce67511c
dinhlegaulois2 <dinh.hu87@yahoo.com>
2018-06-10 12:29:01 -0400

    Add: Steven Grider's Code

--- AdvancedReduxCode-master/auth/server/index.js
+++ AdvancedReduxCode-master/auth/server/index.js
@@ -9,16 +9,15 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 
 // DB Setup
-mongoose.connect('mongodb://localhost:27017/auth');
+mongoose.connect('mongodb://localhost/auth');
 
 // App Setup
 app.use(morgan('combined'));
 app.use(cors());
-app.use(bodyParser.json({ type: '*/*'}));
-
+app.use(bodyParser.json({ type: '*/*' }));
 router(app);
 
-// server Setup
+// Server Setup
 const port = process.env.PORT || 3090;
 const server = http.createServer(app);
 server.listen(port);
