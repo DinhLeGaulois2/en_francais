a522b86106f1c825515e3b6afcc7d73348d4b5ed
dinhlegaulois2 <dinh.hu87@yahoo.com>
2018-08-09 14:22:38 -0400

    Angular - The Complete Guide

---
+++ Angular 6 - The Complete Guide/docSections/_tools/glossary.js
@@ -0,0 +1,6 @@
+const glossary = {
+    title: "", // name of the book, the document, etc.
+    list: [
+        { word: "", explanation: "", ref: "./originalDoc/XXXXXXXX.html#"},
+    ]
+}
---
+++ Angular 6 - The Complete Guide/docSections/originalDoc/Section 2 - The Basics/quiz/assets/script/constants.js
@@ -0,0 +1,5 @@
+const quizConstants = {
+    FORWARD: 0,
+    BACKWARD: 1,
+    REPLAY: 2
+}
